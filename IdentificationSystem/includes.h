105a1aa93f0ad19015d56bbea604018967635c9e
thenerfx <anttholmes@gmail.com>
2022-04-12 23:23:39 +0300

    add

---
+++ IdentificationSystem/includes.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef INCLUDES_H
+#define INCLUDES_H
+
+#include<iostream>
+#include<ctime>
+#include<string>
+#include<typeinfo>
+
+#define SP_BEGIN namespace sp {
+#define SP_END }
+
+#endif
